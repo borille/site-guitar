4ce437005f73e0b09f961acf6565e79a69ae7bfe
borille <thiagoborille@gmail.com>
2013-11-04 09:43:24 -0200

    incluido acl e auth para o modulo admin

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,6 +3,12 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
+	//--------------------------------------------------------------------------
+	//iniciar a configuração do ACL
+	protected function _initAcl()
+	{
+		return new My_Acl_Setup();
+	}
 
 }
 
--- application/modules/admin/Bootstrap.php
+++ application/modules/admin/Bootstrap.php
@@ -1,5 +1,10 @@
 <?php
 class Admin_Bootstrap extends Zend_Application_Module_Bootstrap
 {
-
+	//--------------------------------------------------------------------------
+	//iniciar a configuração do ACL
+	protected function _initAcl()
+	{
+		return new My_Acl_Setup();
+	}
 }
